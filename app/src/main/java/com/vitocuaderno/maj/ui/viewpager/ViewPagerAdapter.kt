1d49db61ebaabea569764a3aed97fe28f3ef68a8
lorrainelanting <ldlanting12@gmail.com>
2021-04-19 09:48:02 +0800

    Implement ViewPager

---
+++ app/src/main/java/com/vitocuaderno/maj/ui/viewpager/ViewPagerAdapter.kt
@@ -0,0 +1,45 @@
+package com.vitocuaderno.maj.ui.viewpager
+
+import android.content.res.Resources
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.vitocuaderno.maj.R
+import com.vitocuaderno.maj.ui.HomeFragment
+import com.vitocuaderno.maj.ui.LoyaltyPointsFragment
+import com.vitocuaderno.maj.ui.OrdersFragment
+import com.vitocuaderno.maj.ui.ProfileFragment
+import com.vitocuaderno.maj.ui.cart.CartFragment
+
+class ViewPagerAdapter constructor (fm: FragmentManager, val resources : Resources) : FragmentPagerAdapter(fm) {
+    companion object {
+        const val NUM_PAGES = 5
+    }
+
+    override fun getCount(): Int =
+        NUM_PAGES
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+//            0 -> HomeFragment()
+            1 -> LoyaltyPointsFragment()
+            2 -> OrdersFragment()
+            3 -> CartFragment()
+            4 -> ProfileFragment()
+            else -> HomeFragment()
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position) {
+            0 -> resources.getString(R.string.app_name)
+            1 -> resources.getString(R.string.points)
+            2 -> resources.getString(R.string.orders)
+            3 -> resources.getString(R.string.cart)
+            4 -> resources.getString(R.string.profile)
+            else -> super.getPageTitle(position)
+        }
+
+    }
+}
