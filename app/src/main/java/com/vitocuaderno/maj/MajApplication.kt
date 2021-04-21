9900f61f8c44e7e2c5f79629ff4ff7aa347ad345
Lorraine Dianne Lanting <lorrainelanting012@gmail.com>
2021-04-21 18:12:59 +0800

    Implement Appplication class.

---
+++ app/src/main/java/com/vitocuaderno/maj/MajApplication.kt
@@ -0,0 +1,17 @@
+package com.vitocuaderno.maj
+
+import android.app.Application
+import android.util.Log
+import com.vitocuaderno.maj.data.AppRoomDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class MajApplication: Application() {
+    private val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { AppRoomDatabase.getDatabase(this, applicationScope) }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("MajApplication", database.toString())
+    }
+}
--- app/src/main/java/com/vitocuaderno/maj/data/model/BaseModel.kt
+++ app/src/main/java/com/vitocuaderno/maj/data/model/BaseModel.kt
@@ -3,7 +3,7 @@ package com.vitocuaderno.maj.data.model
 import androidx.room.ColumnInfo
 import androidx.room.PrimaryKey
 
-open  class BaseModel(@PrimaryKey @ColumnInfo(name = "id") val id: Int = -1) {
+open class BaseModel(@PrimaryKey @ColumnInfo(name = "id") val id: Int = -1) {
     var createdAtTimeStamp: Long = System.currentTimeMillis() / 1000
     var updatedAtTimeStamp: Long = -1
     var deletedAtTimeStamp: Long = -1
