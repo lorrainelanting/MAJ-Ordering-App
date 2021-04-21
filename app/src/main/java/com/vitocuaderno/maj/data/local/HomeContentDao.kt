38763d9c3636d1a200aaea365200b10d995345a0
Lorraine Dianne Lanting <lorrainelanting012@gmail.com>
2021-04-21 18:15:08 +0800

    Directly use LiveData to dao.

--- app/src/main/java/com/vitocuaderno/maj/data/local/HomeContentDao.kt
+++ app/src/main/java/com/vitocuaderno/maj/data/local/HomeContentDao.kt
@@ -1,5 +1,6 @@
 package com.vitocuaderno.maj.data.local
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -9,13 +10,13 @@ import com.vitocuaderno.maj.data.model.HomeContent
 @Dao
 interface HomeContentDao {
     @Query("SELECT * FROM home_content_table ORDER BY id ASC")
-    fun getList(): List<HomeContent>
+    fun getList(): LiveData<List<HomeContent>>
 
     @Query("SELECT * FROM home_content_table WHERE id = :id LIMIT 1")
-    fun getItem(id: Int): HomeContent
+    fun getItem(id: Int): LiveData<HomeContent>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(item: HomeContent)
+    fun insert(item: HomeContent)
 
     @Query("DELETE FROM home_content_table")
     suspend fun deleteAll()
