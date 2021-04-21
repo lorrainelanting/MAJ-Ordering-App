a3bbe309f639495749987b24c8029e6a6e1e7925
Lorraine Dianne Lanting <lorrainelanting012@gmail.com>
2021-04-21 18:13:34 +0800

    Populate database.

--- app/src/main/java/com/vitocuaderno/maj/data/AppRoomDatabase.kt
+++ app/src/main/java/com/vitocuaderno/maj/data/AppRoomDatabase.kt
@@ -10,13 +10,14 @@ import com.vitocuaderno.maj.data.model.HomeContent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-@Database(entities = arrayOf(HomeContent::class), version = 1, exportSchema = false)
-public abstract class AppRoomDatabase : RoomDatabase(), HomeContentDao {
+@Database(entities = [HomeContent::class], version = 1, exportSchema = false)
+public abstract class AppRoomDatabase : RoomDatabase() {
+
     abstract fun homeContentDao(): HomeContentDao
 
     companion object {
         @Volatile
-        private var INSTANCE: AppRoomDatabase? = null
+        var INSTANCE: AppRoomDatabase? = null
 
         fun getDatabase(context: Context, scope: CoroutineScope
         ) : AppRoomDatabase {
@@ -25,7 +26,7 @@ public abstract class AppRoomDatabase : RoomDatabase(), HomeContentDao {
                     context.applicationContext,
                     AppRoomDatabase::class.java,
                     "app_database"
-                ).build()
+                ).addCallback(AppDatabaseCallback(scope)).allowMainThreadQueries().build()
                 INSTANCE = instance
                 // return instance
                 instance
@@ -43,9 +44,55 @@ public abstract class AppRoomDatabase : RoomDatabase(), HomeContentDao {
             }
         }
 
-        suspend fun populateDatabase(homeContentDao: HomeContentDao) {
-            var homeContent = HomeContent(1)
-//            homeContentDao.insert()
+        suspend fun populateDatabase(dao: HomeContentDao) {
+            // Delete all content here.
+            dao.deleteAll()
+
+            var list = mutableListOf<HomeContent>()
+
+            list.add(HomeContent.newInstance(
+                id = 1,
+                productImgUrl = "https://www.rakhoiwholesale.com/wp-content/uploads/2017/10/coca-cola-bottle-1.5l-block-247x247.jpg",
+                productDescription = "Coke 1.5L",
+                productUnitCost = 658.00
+            ))
+
+            list.add(HomeContent.newInstance(
+                id = 2,
+                productImgUrl = "https://images-na.ssl-images-amazon.com/images/I/51FN8q2j5KL._AC_UL600_SR600,600_.jpg",
+                productDescription = "Sprite 1.5L",
+                productUnitCost = 658.00
+            ))
+
+            list.add(HomeContent.newInstance(
+                id = 3,
+                productImgUrl = "https://ph-test-11.slatic.net/p/7115bea5d01a66cc06fd1c914c9ff2cf.jpg",
+                productDescription = "Royal 1.5L",
+                productUnitCost = 658.00
+            ))
+
+            list.add(HomeContent.newInstance(
+                id = 4,
+                productImgUrl = "https://lookingfourapp.s3-ap-southeast-1.amazonaws.com/d_ii/ii_10479_5af0080f4dc0545fca0f94adc39ef1d7.jpg",
+                productDescription = "Coke Mismo 295ml",
+                productUnitCost = 137.00
+            ))
+
+            list.add(HomeContent.newInstance(
+                id = 5,
+                productImgUrl = "https://ph-test-11.slatic.net/p/8cab9dc22fa5f80220652a8aa78ade7f.jpg",
+                productDescription = "Sprite Mismo 295ml",
+                productUnitCost = 137.00
+            ))
+
+            list.add(HomeContent.newInstance(
+                id = 6,
+                productImgUrl = "https://lookingfourapp.s3-ap-southeast-1.amazonaws.com/d_ii/ii_10488_7b5ffeda48cb8f11b918a6239b0463d2.jpg",
+                productDescription = "Royal Mismo 295ml",
+                productUnitCost = 137.00
+            ))
+
+            list.forEach { dao.insert(it) }
         }
     }
 }
