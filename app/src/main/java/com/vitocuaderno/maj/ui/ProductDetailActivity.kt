d6664d49874405f7e1d48d57dbc25bd9a5d12005
Lorraine Dianne Lanting <lorrainelanting012@gmail.com>
2021-04-21 18:21:00 +0800

    Bind getItem by id to view from repository.

--- app/src/main/java/com/vitocuaderno/maj/ui/ProductDetailActivity.kt
+++ app/src/main/java/com/vitocuaderno/maj/ui/ProductDetailActivity.kt
@@ -1,35 +1,54 @@
 package com.vitocuaderno.maj.ui
 
 import android.os.Bundle
-import android.widget.Toast
+import androidx.lifecycle.LiveData
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import com.vitocuaderno.maj.R
 import com.vitocuaderno.maj.data.model.HomeContent
+import com.vitocuaderno.maj.data.repository.HomeContentRepository
+import com.vitocuaderno.maj.data.util.CurrencyUtil
 import com.vitocuaderno.maj.databinding.ActivityProductDetailBinding
+import com.vitocuaderno.maj.di.Injection
 
 class ProductDetailActivity : BaseActivity<ActivityProductDetailBinding>() {
+    lateinit var repository: HomeContentRepository
+
+    lateinit var homeContentLiveData: LiveData<HomeContent>
+
     override fun getLayoutId(): Int = R.layout.activity_product_detail
+
+    companion object {
+        const val ID = "id"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        repository = Injection.provideHomeContentRepository(this)
         intent?.extras?.let {
-            Toast.makeText(this, it.getInt("id").toString(), Toast.LENGTH_SHORT).show()
+            homeContentLiveData = repository.getItem(it.getInt(ID))
+            homeContentLiveData.observe(this) { it ->
+                bindData(it)
+            }
         }
+    }
+
+    private fun bindData(homeContent: HomeContent) {
         //  Load image from url string
         val picasso = Picasso.get()
-        for (i in 0..10) {
-            val content : HomeContent = HomeContent(i)
-            picasso.load(content.productImgUrl).error(R.drawable.ic_homepage).into(
-                binding.imgProduct,
-                object : Callback {
-                    override fun onSuccess() {}
-
-                    override fun onError(e: Exception?) {
-                        e?.printStackTrace()
-                    }
+        picasso.load(homeContent.productImgUrl).error(R.drawable.ic_homepage).into(
+            binding.imgProduct,
+            object : Callback {
+                override fun onSuccess() {}
+                override fun onError(e: Exception?) {
+                    e?.printStackTrace()
                 }
-            )
-        }
+            }
+        )
+
+        binding.txtProductName.text = homeContent.productDescription
+        binding.txtUnitCost.text = CurrencyUtil.format(homeContent.productUnitCost)
+        binding.txtProductDescription.text = homeContent.productDescription
+        binding.txtProductPackQty.text = homeContent.productPackQty
     }
 }
