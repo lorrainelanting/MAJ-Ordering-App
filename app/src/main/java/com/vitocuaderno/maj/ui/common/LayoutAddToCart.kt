63c6f2de624cbee17168c5efb0a421a5e1981523
lorrainelanting <ldlanting12@gmail.com>
2021-04-26 14:41:51 +0800

    Bind data product model.

--- app/src/main/java/com/vitocuaderno/maj/ui/common/LayoutAddToCart.kt
+++ app/src/main/java/com/vitocuaderno/maj/ui/common/LayoutAddToCart.kt
@@ -8,10 +8,17 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
+import com.squareup.picasso.Picasso
 import com.vitocuaderno.maj.R
+import com.vitocuaderno.maj.data.model.Product
+import com.vitocuaderno.maj.data.util.CurrencyUtil
 
 class LayoutAddToCart(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
     private val layoutAddToCartListener: LayoutAddToCartListener? = null
+
+    companion object {
+        const val ID = "id"
+    }
 
     init {
         inflate(context, R.layout.layout_add_to_cart, this)
@@ -19,9 +26,6 @@ class LayoutAddToCart(context: Context, attrs: AttributeSet) : FrameLayout(conte
         //Custom attributes
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.LayoutAddToCart)
 
-        val imgProduct = findViewById<ImageView>(R.id.imgProduct)
-        val unitProductPrice = findViewById<TextView>(R.id.txtUnitCost)
-        val productDescription = findViewById<TextView>(R.id.txtProductDescription)
         val productQuantity = findViewById<TextView>(R.id.txtProductQty)
         val btnMinus = findViewById<FrameLayout>(R.id.btnMinus)
         val btnAdd = findViewById<FrameLayout>(R.id.btnAdd)
@@ -30,9 +34,6 @@ class LayoutAddToCart(context: Context, attrs: AttributeSet) : FrameLayout(conte
         val frameLayout = findViewById<FrameLayout>(R.id.frameAddToCartLayout)
         val customSnackBar = findViewById<ConstraintLayout>(R.id.clSnackBar)
 
-        imgProduct.setImageDrawable(attributes.getDrawable(R.styleable.LayoutAddToCart_img_product))
-        unitProductPrice.text = attributes.getString(R.styleable.LayoutAddToCart_txt_unit_price)
-        productDescription.text = attributes.getString(R.styleable.LayoutAddToCart_txt_product_description)
         productQuantity.text = attributes.getString(R.styleable.LayoutAddToCart_txt_quantity)
 
         // Consume touch event stop propagation
@@ -69,6 +70,17 @@ class LayoutAddToCart(context: Context, attrs: AttributeSet) : FrameLayout(conte
 
     private fun close() {
         this.isVisible = false
+    }
+
+    fun bind(product: Product) {
+        val picasso = Picasso.get()
+        val imgProduct = findViewById<ImageView>(R.id.imgProduct)
+        val unitProductPrice = findViewById<TextView>(R.id.txtUnitCost)
+        val productDescription = findViewById<TextView>(R.id.txtProductDescription)
+
+        picasso.load(product.productImgUrl).placeholder(R.color.colorSecondary).error(R.drawable.ic_soft_drink).into(imgProduct)
+        unitProductPrice.text = CurrencyUtil.format(product.productUnitCost)
+        productDescription.text = "${product.productDescription} ${product.productPackQty}"
     }
 
     interface LayoutAddToCartListener {
