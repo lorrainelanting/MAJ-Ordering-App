1c369b1a5a93b91600c0df3acc281e324d883082
lorrainelanting <ldlanting12@gmail.com>
2021-04-30 14:01:46 +0800

    Implement auto generated primary key.

--- app/src/main/java/com/vitocuaderno/maj/data/AppRoomDatabase.kt
+++ app/src/main/java/com/vitocuaderno/maj/data/AppRoomDatabase.kt
@@ -5,15 +5,18 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.vitocuaderno.maj.data.local.CartContentDao
 import com.vitocuaderno.maj.data.local.ProductDao
+import com.vitocuaderno.maj.data.model.CartContent
 import com.vitocuaderno.maj.data.model.Product
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-@Database(entities = [Product::class], version = 1, exportSchema = false)
+@Database(entities = [Product::class, CartContent::class], version = 1, exportSchema = false)
 public abstract class AppRoomDatabase : RoomDatabase() {
 
     abstract fun productDao(): ProductDao
+    abstract fun cartContentDao(): CartContentDao
 
     companion object {
         @Volatile
@@ -51,42 +54,42 @@ public abstract class AppRoomDatabase : RoomDatabase() {
             var list = mutableListOf<Product>()
 
             list.add(Product.newInstance(
-                id = 1,
+                productName = "Coke 1.5L",
                 productImgUrl = "https://www.rakhoiwholesale.com/wp-content/uploads/2017/10/coca-cola-bottle-1.5l-block-247x247.jpg",
                 productDescription = "Coke 1.5L",
                 productUnitCost = 658.00
             ))
 
             list.add(Product.newInstance(
-                id = 2,
+                productName = "Sprite 1.5L",
                 productImgUrl = "https://images-na.ssl-images-amazon.com/images/I/51FN8q2j5KL._AC_UL600_SR600,600_.jpg",
                 productDescription = "Sprite 1.5L",
                 productUnitCost = 658.00
             ))
 
             list.add(Product.newInstance(
-                id = 3,
+                productName = "Royal 1.5L",
                 productImgUrl = "https://ph-test-11.slatic.net/p/7115bea5d01a66cc06fd1c914c9ff2cf.jpg",
                 productDescription = "Royal 1.5L",
                 productUnitCost = 658.00
             ))
 
             list.add(Product.newInstance(
-                id = 4,
+                productName = "Coke Mismo 295ml",
                 productImgUrl = "https://lookingfourapp.s3-ap-southeast-1.amazonaws.com/d_ii/ii_10479_5af0080f4dc0545fca0f94adc39ef1d7.jpg",
                 productDescription = "Coke Mismo 295ml",
                 productUnitCost = 137.00
             ))
 
             list.add(Product.newInstance(
-                id = 5,
+                productName = "Sprite Mismo 295ml",
                 productImgUrl = "https://ph-test-11.slatic.net/p/8cab9dc22fa5f80220652a8aa78ade7f.jpg",
                 productDescription = "Sprite Mismo 295ml",
                 productUnitCost = 137.00
             ))
 
             list.add(Product.newInstance(
-                id = 6,
+                productName = "Royal Mismo 295ml",
                 productImgUrl = "https://lookingfourapp.s3-ap-southeast-1.amazonaws.com/d_ii/ii_10488_7b5ffeda48cb8f11b918a6239b0463d2.jpg",
                 productDescription = "Royal Mismo 295ml",
                 productUnitCost = 137.00
