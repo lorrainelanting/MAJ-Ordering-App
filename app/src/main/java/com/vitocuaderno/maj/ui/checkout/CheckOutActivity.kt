747107bb9808743949eeceba1569c1cdcbf058a4
lorrainelanting <ldlanting12@gmail.com>
2021-05-10 16:12:30 +0800

    Implement data view binding and calculations.

--- app/src/main/java/com/vitocuaderno/maj/ui/checkout/CheckOutActivity.kt
+++ app/src/main/java/com/vitocuaderno/maj/ui/checkout/CheckOutActivity.kt
@@ -1,15 +1,18 @@
 package com.vitocuaderno.maj.ui.checkout
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.lifecycle.observe
 import com.vitocuaderno.maj.R
+import com.vitocuaderno.maj.data.model.CartContent
+import com.vitocuaderno.maj.data.util.CurrencyUtil
 import com.vitocuaderno.maj.databinding.ActivityCheckOutBinding
 import com.vitocuaderno.maj.ui.BaseActivity
+import com.vitocuaderno.maj.ui.OrderAdapter
 
-class CheckOutActivity : BaseActivity<ActivityCheckOutBinding>() {
-    companion object{
-        const val ID_ARR = "idArray"
-    }
+class CheckOutActivity : BaseActivity<ActivityCheckOutBinding>(),
+    OrderAdapter.OrderAdapterCalculation {
+    var adapter: OrderAdapter? = null
 
     private val viewModel = CheckOutViewModel()
 
@@ -17,11 +20,33 @@ class CheckOutActivity : BaseActivity<ActivityCheckOutBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        adapter = OrderAdapter(emptyList(), this)
+        binding.layoutOrderSummary.rvCheckOutProduct.adapter = adapter
         viewModel.injectCart(this)
-        intent?.extras?.let {
-            viewModel.cartContentsLiveData.observe(this) { it ->
-            }
-        }
+        viewModel.cartContentsLiveData.observe(this) { cartContents ->
+            adapter?.update(cartContents)
 
+            binding.layoutSummaryFees.txtResultSubTotal.text =
+                CurrencyUtil.format(computeSubtotal(cartContents))
+            binding.frameSummaryCheckout.findViewById<TextView>(R.id.txtResultTotalPayment).text =
+                CurrencyUtil.format(computeTotalPayment(computeSubtotal(cartContents), 0.00))
+        }
+    }
+
+    override fun getTotal(unitPrice: Double, quantity: Int): Double {
+        return unitPrice * quantity
+    }
+
+    private fun computeSubtotal(list: List<CartContent>): Double {
+        var subtotal = 0.00
+        for (item in list) {
+            val total = item.productUnitCost * item.quantity
+            subtotal += total
+        }
+        return subtotal
+    }
+
+    private fun computeTotalPayment(subtotal: Double, shippingFee: Double): Double {
+        return subtotal + shippingFee
     }
 }
