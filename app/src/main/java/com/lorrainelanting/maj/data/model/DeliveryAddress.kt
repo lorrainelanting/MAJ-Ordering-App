ff581090683dcabf21056ea6fa19443a80dbdbae
lorrainelanting <ldlanting12@gmail.com>
2021-05-19 14:55:07 +0800

    Remove instance on delivery address.

--- app/src/main/java/com/lorrainelanting/maj/data/model/DeliveryAddress.kt
+++ app/src/main/java/com/lorrainelanting/maj/data/model/DeliveryAddress.kt
@@ -7,15 +7,4 @@ class DeliveryAddress: BaseModel() {
     var city: String = ""
     var barangay: String = ""
     var streetName: String = ""
-
-    companion object {
-        fun newInstance(city: String, barangay: String, streetName: String): DeliveryAddress {
-            val deliveryAddress = DeliveryAddress()
-            deliveryAddress.city = city
-            deliveryAddress.barangay = barangay
-            deliveryAddress.streetName = streetName
-
-            return deliveryAddress
-        }
-    }
 }
--- app/src/main/java/com/lorrainelanting/maj/data/model/User.kt
+++ app/src/main/java/com/lorrainelanting/maj/data/model/User.kt
@@ -4,8 +4,10 @@ import androidx.room.Entity
 
 @Entity(tableName = "user_table")
 class User: BaseModel() {
-    var lastName: String = ""
-    var firstName: String = ""
-    var middleInitial: String = ""
-    var contactNum: Int = 0
+    var fullName: String = ""
+//    var lastName: String = ""
+//    var firstName: String = ""
+//    var middleInitial: String = ""
+    var contactNum: Long = 0L
+    var storeName: String = ""
 }
