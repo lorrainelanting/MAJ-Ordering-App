79a2dac53ef86cafa8ba150dcafdf2b903f36214
lorrainelanting <ldlanting12@gmail.com>
2021-05-19 14:56:02 +0800

    Rename Constants currency; Add new constants.

--- app/src/main/java/com/lorrainelanting/maj/data/util/Constants.kt
+++ app/src/main/java/com/lorrainelanting/maj/data/util/Constants.kt
@@ -2,6 +2,15 @@ package com.lorrainelanting.maj.data.util
 
 class Constants {
     companion object {
-        const val currency: String = "₱"
+        const val CURRENCY: String = "₱"
+
+        // Delivery Options
+        const val DELIVER: Int = 1
+        const val PICK_UP: Int = 2
+
+        // Order Status
+        const val DELIVERED: String = "Delivered"
+        const val PICKED_UP: String = "Picked-Up"
+        const val PREPARING: String = "Preparing"
     }
 }
--- app/src/main/java/com/lorrainelanting/maj/data/util/CurrencyUtil.kt
+++ app/src/main/java/com/lorrainelanting/maj/data/util/CurrencyUtil.kt
@@ -6,7 +6,7 @@ class CurrencyUtil {
     companion object {
         fun format(amount: Double) : String {
             val formatter = DecimalFormat("###,###,##0.00")
-            return "${Constants.currency} ${formatter.format(amount)}"
+            return "${Constants.CURRENCY} ${formatter.format(amount)}"
         }
     }
 }
