5ffc3afece568da293a73ae5fa4f6902760ef443
lorrainelanting <ldlanting12@gmail.com>
2021-05-24 17:56:31 +0800

    Refactor file name.

--- app/src/main/java/com/lorrainelanting/maj/ui/checkout/CheckOutOrderSummaryContentAdapter.kt
+++ app/src/main/java/com/lorrainelanting/maj/ui/checkout/CheckOutOrderSummaryContentAdapter.kt
@@ -1,4 +1,4 @@
-package com.lorrainelanting.maj.ui
+package com.lorrainelanting.maj.ui.order
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
@@ -8,14 +8,14 @@ import com.squareup.picasso.Picasso
 import com.lorrainelanting.maj.R
 import com.lorrainelanting.maj.data.model.CartContent
 import com.lorrainelanting.maj.data.util.CurrencyUtil
-import com.lorrainelanting.maj.databinding.ItemOrderBinding
+import com.lorrainelanting.maj.databinding.ItemCheckoutOrderSummaryBinding
 
 class OrderAdapter(
     private var dataSet: List<CartContent>,
     private val orderAdapterCalculation: OrderAdapterCalculation? = null
 ) : RecyclerView.Adapter<OrderAdapter.OrderViewHolder>() {
     class OrderViewHolder(
-        private val binding: ItemOrderBinding,
+        private val binding: ItemCheckoutOrderSummaryBinding,
         private val orderAdapterCalculation: OrderAdapterCalculation? = null
     ) : RecyclerView.ViewHolder(binding.root) {
         @SuppressLint("SetTextI18n")
@@ -34,8 +34,11 @@ class OrderAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
-        val order = ItemOrderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return OrderViewHolder(order, orderAdapterCalculation)
+        val order = ItemCheckoutOrderSummaryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return OrderViewHolder(
+            order,
+            orderAdapterCalculation
+        )
     }
 
     override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
