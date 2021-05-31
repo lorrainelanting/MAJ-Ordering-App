2b63b1ba345b8a4903961fe627455a7763f01e7d
lorrainelanting <ldlanting12@gmail.com>
2021-05-31 14:47:22 +0800

    Add layout for incomplete user info; Add select delivery date UI.

--- app/src/main/java/com/lorrainelanting/maj/ui/checkout/CheckOutActivity.kt
+++ app/src/main/java/com/lorrainelanting/maj/ui/checkout/CheckOutActivity.kt
@@ -1,48 +1,54 @@
 package com.lorrainelanting.maj.ui.checkout
 
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.observe
 import com.lorrainelanting.maj.R
 import com.lorrainelanting.maj.data.model.Order
 import com.lorrainelanting.maj.data.model.User
+import com.lorrainelanting.maj.data.util.Constants
 import com.lorrainelanting.maj.data.util.CurrencyUtil
+import com.lorrainelanting.maj.data.util.DateUtil
+import com.lorrainelanting.maj.data.util.NetworkConnectivity
 import com.lorrainelanting.maj.databinding.ActivityCheckOutBinding
 import com.lorrainelanting.maj.di.Injection
 import com.lorrainelanting.maj.ui.base.BaseActivity
+import java.util.*
 
 
 class CheckOutActivity : BaseActivity<ActivityCheckOutBinding>(),
     CheckOutOrderSummaryContentAdapter.CheckOutOrderSummaryContentAdapterCalculation {
-    var adapter: CheckOutOrderSummaryContentAdapter? = null
 
+    var adapter: CheckOutOrderSummaryContentAdapter? = null
     lateinit var viewModel: CheckOutViewModel
     lateinit var customerInfo: User
     lateinit var delAddress: String
-
+    var deliveryDate: Long = 0
+    private lateinit var networkConnectivity: NetworkConnectivity
+    lateinit var selectedDeliveryDate: Date
 
     companion object {
-        const val SOURCE = "source"
-
-        const val PROFILE_REQ = 1
-
-//        val URI = Uri.parse("fb-messenger://user/majplacelipa")
+        const val MAJ_CONTACT_NUM = "09178913668"
     }
 
     override fun getLayoutId(): Int = R.layout.activity_check_out
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        networkConnectivity = NetworkConnectivity()
+        selectedDeliveryDate = Date()
         adapter =
             CheckOutOrderSummaryContentAdapter(emptyList(), this)
+
         binding.layoutOrderSummary.rvCheckOutProduct.adapter = adapter
 
         viewModel = CheckOutViewModel(
@@ -54,17 +60,40 @@ class CheckOutActivity : BaseActivity<ActivityCheckOutBinding>(),
         )
 
         viewModel.userLiveData.observe(this) { list ->
+            if (list.isNotEmpty()) {
+                binding.layoutUserBanner.root.visibility = View.GONE
+                binding.layoutCustomerDetail.root.visibility = View.VISIBLE
+            } else {
+                binding.layoutUserBanner.root.visibility = View.VISIBLE
+                binding.layoutCustomerDetail.root.visibility = View.GONE
+            }
+
+            binding.layoutUserBanner.btnUpdateNow.setOnClickListener {
+                setResult(4)
+                finish()
+            }
+
             for (user in list) {
+                if (user.contactNum != "" || user.fullName != "") {
+                    binding.layoutUserBanner.root.visibility = View.GONE
+                    binding.layoutCustomerDetail.root.visibility = View.VISIBLE
+                } else {
+                    binding.layoutUserBanner.root.visibility = View.VISIBLE
+                    binding.layoutCustomerDetail.root.visibility = View.GONE
+                }
+
                 customerInfo = user
                 binding.layoutCustomerDetail.txtCustomerName.text = user.fullName
                 binding.layoutCustomerDetail.txtStoreName.text = user.storeName
-                binding.layoutCustomerDetail.txtCustomerMobile.setText(user.contactNum.toString())
+                binding.layoutCustomerDetail.txtCustomerMobile.setText(user.contactNum)
             }
+            deliveryDate = DateUtil.formatToDate(selectedDeliveryDate)
         }
 
         viewModel.deliveryAddressLiveData.observe(this) { list ->
             for (deliveryAddress in list) {
-                delAddress = "${deliveryAddress.streetName}, ${deliveryAddress.barangay}, ${deliveryAddress.city}"
+                delAddress =
+                    "${deliveryAddress.streetName}, ${deliveryAddress.barangay}, ${deliveryAddress.city}"
                 binding.layoutCustomerDetail.txtCustomerAddress.text = delAddress
 
             }
@@ -93,30 +122,20 @@ class CheckOutActivity : BaseActivity<ActivityCheckOutBinding>(),
 
                 binding.btnPlaceOrder.setOnClickListener {
                     val positiveBtnClick = { dialog: DialogInterface, which: Int ->
-                        for (cartContent in cartContents) {
-                            var customerDetails = getString(R.string.customer_details, customerInfo.fullName, delAddress, customerInfo.contactNum.toString())
-                            val orderDetails = getString(R.string.order_details, cartContent.product.name, cartContent.cartContent.quantity.toString(), "30-May-2021", "0.00", "100.00")
-
-                            val uri = Uri.parse("https://www.facebook.com/majplacelipa/")
-                            val page = Uri.parse("fb://page/104399324329005")
-//                            106709074945805 page.debugger
-
-                            val page1 = Uri.parse("fb://user/page.debugger")
-                            val intent = Intent(Intent.ACTION_SEND, page)
-                                .setType("text/plain")
-                                .setPackage("com.facebook.orca")
-                                .putExtra(Intent.EXTRA_TEXT, orderDetails)
-
-                            if (intent.resolveActivity(this.packageManager) != null) {
-                                startActivityForResult(intent, 2)
-
-                                val order: Order = viewModel.ordersContentNewInstance("1", cartContent)
-                                viewModel.orderRepository.save(order)
-
-                                setResult(2)
-                                finish()
+                        for ((i, cartContent) in cartContents.withIndex()) {
+                            val deliveryOption =
+                                binding.layoutDeliveryDetails.rgDeliveryOption.checkedRadioButtonId
+                            val status = Constants.PLACED_ORDER
+                            val isConnected = networkConnectivity.execute()
+                            if (isConnected.get()) {
+                                setOrder(i.toString(), deliveryOption, status, cartContent)
                             } else {
-                                Toast.makeText(this, "No app found", Toast.LENGTH_SHORT).show()
+                                onNetworkNotAvailable(
+                                    cartContent,
+                                    i.toString(),
+                                    deliveryOption,
+                                    status
+                                )
                             }
                         }
                     }
@@ -126,7 +145,6 @@ class CheckOutActivity : BaseActivity<ActivityCheckOutBinding>(),
 
                     alert(positiveBtnClick, negativeBtnClick)
                 }
-
             }
         }
 
@@ -135,9 +153,60 @@ class CheckOutActivity : BaseActivity<ActivityCheckOutBinding>(),
             finish()
         }
 
-        binding.layoutDeliveryOpt.rgDeliveryOption.setOnCheckedChangeListener { group, checkedId ->
+        binding.layoutDeliveryDetails.rgDeliveryOption.setOnCheckedChangeListener { group, checkedId ->
             onRadioBtnClicked(group)
         }
+
+        binding.layoutDeliveryDetails.imgEditCalendar.setOnClickListener {
+            onSetDateClicked()
+        }
+
+        binding.layoutDeliveryDetails.txtSelectDate.setOnClickListener {
+            onSetDateClicked()
+        }
+    }
+
+    private fun showSelectedDate() {
+        val viewSelectedDate = binding.layoutDeliveryDetails.txtSelectedDate
+        val txtSelectDate = binding.layoutDeliveryDetails.txtSelectDate
+        val txtLabelSelectedDate = binding.layoutDeliveryDetails.txtLabelSelectedDate
+
+        if (viewSelectedDate.text != "") {
+            txtLabelSelectedDate.visibility = View.VISIBLE
+            txtSelectDate.visibility = View.GONE
+        } else {
+            txtLabelSelectedDate.visibility = View.GONE
+            txtSelectDate.visibility = View.VISIBLE
+        }
+    }
+
+    private fun onSetDateClicked() {
+        val viewSelectedDate = binding.layoutDeliveryDetails.txtSelectedDate
+        val cal = Calendar.getInstance()
+
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                // bind selected date
+                viewSelectedDate.text = DateUtil.formatToString(cal.time)
+                selectedDeliveryDate = cal.time
+                showSelectedDate()
+            }
+
+        val datePickerDialog = DatePickerDialog(
+            this, dateSetListener,
+            cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH),
+            cal.get(Calendar.DAY_OF_MONTH)
+        )
+
+        // Set minimum date to current date
+        datePickerDialog.datePicker.minDate = System.currentTimeMillis()
+
+        datePickerDialog.show()
     }
 
     override fun getTotal(unitPrice: Double, quantity: Int): Double {
@@ -188,5 +257,60 @@ class CheckOutActivity : BaseActivity<ActivityCheckOutBinding>(),
 
         negativeBtn.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary))
         positiveBtn.setTextColor(ContextCompat.getColor(this, R.color.colorSecondaryVariant))
+    }
+
+    private fun setOrder(
+        id: String,
+        deliveryOption: Int,
+        status: String,
+        content: CheckOutOrderSummaryContentAdapter.Content,
+    ) {
+        val order: Order = viewModel.ordersContentNewInstance(
+            id,
+            deliveryOption,
+            status,
+            content,
+            customerInfo,
+            delAddress,
+            deliveryDate
+        )
+        viewModel.orderRepository.save(order)
+
+        setResult(2)
+        finish()
+    }
+
+    private fun onNetworkNotAvailable(
+        cartContent: CheckOutOrderSummaryContentAdapter.Content,
+        id: String,
+        deliveryOption: Int,
+        status: String,
+    ) {
+
+        val selectedDeliveryOption =
+            binding.layoutDeliveryDetails.rgDeliveryOption.checkedRadioButtonId
+        val selectedDeliveryText = findViewById<RadioButton>(selectedDeliveryOption).text.toString()
+        val smsOrderText = getString(
+            R.string.sms_order,
+            selectedDeliveryText,
+            cartContent.cartContent.quantity.toString(),
+            cartContent.product.name,
+            Constants.DELIVERY_DATE,
+            "30-May-2021",
+            customerInfo.fullName,
+            customerInfo.contactNum,
+            delAddress
+        )
+
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            type = "text/plain"
+            data = Uri.parse("smsto: $MAJ_CONTACT_NUM")
+            putExtra(Intent.EXTRA_TEXT, smsOrderText)
+        }
+
+        if (intent.resolveActivity(this.packageManager) != null) {
+            startActivity(intent)
+            setOrder(id, deliveryOption, status, cartContent)
+        }
     }
 }
--- app/src/main/java/com/lorrainelanting/maj/ui/checkout/CheckOutViewModel.kt
+++ app/src/main/java/com/lorrainelanting/maj/ui/checkout/CheckOutViewModel.kt
@@ -2,12 +2,12 @@ package com.lorrainelanting.maj.ui.checkout
 
 import androidx.lifecycle.ViewModel
 import com.lorrainelanting.maj.data.model.Order
+import com.lorrainelanting.maj.data.model.User
 import com.lorrainelanting.maj.data.repository.cart.CartRepository
 import com.lorrainelanting.maj.data.repository.deliveryaddress.DeliveryAddressRepository
 import com.lorrainelanting.maj.data.repository.order.OrderRepository
 import com.lorrainelanting.maj.data.repository.product.ProductRepository
 import com.lorrainelanting.maj.data.repository.user.UserRepository
-import com.lorrainelanting.maj.data.util.Constants
 
 class CheckOutViewModel(
     val cartRepository: CartRepository,
@@ -23,17 +23,21 @@ class CheckOutViewModel(
 
     val cartContentsLiveData by lazy { cartRepository.getList() }
 
-    fun ordersContentNewInstance(id: String, content: CheckOutOrderSummaryContentAdapter.Content): Order {
+    fun ordersContentNewInstance(id: String, deliveryOption: Int, status: String, content: CheckOutOrderSummaryContentAdapter.Content, customer: User, deliveryAddress: String, deliveryDate: Long): Order {
         return Order.newInstance(
             id = id,
-            deliveryOption = Constants.DELIVER,
-            status = Constants.DELIVERED,
+            deliveryOption = deliveryOption,
+            status = status,
             productId = content.cartContent.productId,
             productName = content.product.name,
             productImgUrl = content.product.imgUrl,
             quantity = content.cartContent.quantity,
             productPrice = content.product.price,
-            productPackQty = content.product.packQty
+            productPackQty = content.product.packQty,
+            customerName = customer.fullName,
+            customerContactNum = customer.contactNum,
+            deliveryAddress = deliveryAddress,
+            deliveryDate = deliveryDate
         )
     }
 }
