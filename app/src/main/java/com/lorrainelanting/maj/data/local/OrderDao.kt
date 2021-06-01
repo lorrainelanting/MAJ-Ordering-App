7a7b5e419978664d566bf3b725ccf4c6345ef204
lorrainelanting <ldlanting12@gmail.com>
2021-06-01 10:38:41 +0800

    Remove order.id to newInstance; Rename constants variable.

--- app/src/main/java/com/lorrainelanting/maj/data/local/OrderDao.kt
+++ app/src/main/java/com/lorrainelanting/maj/data/local/OrderDao.kt
@@ -9,6 +9,9 @@ interface OrderDao {
     @Query("SELECT * FROM order_table ORDER BY createdAtTimeStamp ASC")
     fun getList(): LiveData<List<Order>>
 
+    @Query("SELECT * FROM order_table ORDER BY createdAtTimeStamp ASC")
+    fun getOrders(): List<Order>
+
     @Query("SELECT * FROM order_table WHERE id = :id LIMIT 1")
     fun getItem(id: String): LiveData<Order>
 
--- app/src/main/java/com/lorrainelanting/maj/data/util/Constants.kt
+++ app/src/main/java/com/lorrainelanting/maj/data/util/Constants.kt
@@ -5,16 +5,16 @@ class Constants {
         const val CURRENCY: String = "₱"
 
         // Delivery Options
-        const val DELIVER: Int = 1
-        const val PICK_UP: Int = 2
+        const val OPTION_DELIVER: Int = 1
+        const val OPTION_PICK_UP: Int = 2
 
         const val DELIVERY_DATE: String = "Delivery Date"
         const val PICK_UP_DATE: String = "Pick-up Date"
 
         // Order Status
-        const val DELIVERED: String = "Delivered"
-        const val PICKED_UP: String = "Picked-Up"
-        const val PLACED_ORDER = "Placed Order"
+        const val STATUS_DELIVERED: String = "Delivered"
+        const val STATUS_PICKED_UP: String = "Picked-Up"
+        const val STATUS_PLACED_ORDER = "Placed Order"
 
         // Date Format
         const val DATE_FORMAT = "dd MMM yyyy" // 01 Jan 2021
